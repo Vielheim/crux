9e50ea470c4b29891de6a28631dd7490ab162353
Vielheim <terencehwy@gmail.com>
2026-02-01 16:10:22 +0800

    Update docker mapping for FE

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true, // Crucial: Listen on 0.0.0.0 for Docker
+    strictPort: true,
+    port: 5173,
+    watch: {
+      usePolling: true, // Fixes HMR file watching in some Docker environments
+    },
+  }
 })
